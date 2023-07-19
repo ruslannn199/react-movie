f0f899844c71d5b76ee8460eed5ee1d1a8ebfc69
ruslannn199 <ruslannn199@gmail.com>
2023-07-19 14:05:07 +0300

    feat: make header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+// Components
+import Header from './components/Header';
+import Home from './components/Home';
 
 // Styles
 import { GlobalStyle } from './GlobalStyles'; 
@@ -6,7 +9,8 @@ import { GlobalStyle } from './GlobalStyles';
 const App = () => {
   return (
     <div className="App">
-      Start here.
+      <Header />
+      <Home />
       <GlobalStyle />
     </div>
   );
