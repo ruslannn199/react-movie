7ed8b6654435cc7a95476f6c51be18e05be8be55
ruslannn199 <ruslannn199@gmail.com>
2023-07-19 12:06:58 +0300

    init: start project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Styles
+import { GlobalStyle } from './GlobalStyles'; 
+
+const App = () => {
+  return (
+    <div className="App">
+      Start here.
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
